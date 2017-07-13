eb76b34f6e7e2adf45ef46161e70e9bcaf18017e
DESKTOP-24IHVPG\J <wjdwo0106@gmail.com>
2017-07-13 15:05:49 +0900

    vue resource 설정

--- my-profile/src/main.js
+++ my-profile/src/main.js
@@ -1,9 +1,11 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import {routes} from './routes'
+import VueResource from 'vue-resource'
 import App from './App.vue'
 
 Vue.use(VueRouter);
+Vue.use(VueResource);
 
 const router = new VueRouter({
 	routes
